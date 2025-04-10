6d105a90220d898ccc38b77251f4bd95610b3934
S SIDHARTH <sidharthpunalur@gmail.com>
2025-04-10 08:51:20 +0530

    Project base added

---
+++ src/components/landing/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+        return <h1>IEDC CET</h1>;
+      }
+      export default Header;
+      
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/landing/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <p>Welcome to /pages</p>
+    </>
+  );
+}
+export default Home;
