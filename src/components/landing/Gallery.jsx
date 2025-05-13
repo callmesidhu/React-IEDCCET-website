37acf900df81eeb68ef4c915b3ac472a70f3cd00
Suraj12P <surajpmnr@gmail.com>
2025-05-13 16:41:39 +0530

    gallery,announcement,about page dark mode added

--- src/components/landing/Gallery.jsx
+++ src/components/landing/Gallery.jsx
@@ -7,6 +7,7 @@ import "swiper/css/pagination";
 import aboutimg from "../../assets/About.png";
 import facultyimg from "../../assets/Faculty.png";
 import { motion } from "framer-motion";
+import { useDarkMode } from "../../context/DarkModeContext"; 
 
 export default function Gallery() {
   const images = [
@@ -15,30 +16,39 @@ export default function Gallery() {
     { id: 3, src: aboutimg, alt: "Collaboration" },
   ];
 
+  const { darkMode, setDarkMode } = useDarkMode();
+    const bgColor = darkMode ? "#000414" : "#FFFFFF";
+    
   return (
-<div className="w-full min-h-screen mx-auto p-6 flex flex-col items-center relative mb-20 overflow-x-hidden">
+    <div
+      className="w-full min-h-[120vh] mx-auto  flex flex-col items-center relative px-4 pt-10 pb-20 overflow-hidden"
+      style={{ backgroundColor: bgColor }}
+    >
       <img
         src="/green.png"
         alt=""
-        className="absolute left-[0px] -top-[-47px] w-[170px] h-[180px] object-contain"
+        className="absolute left-[-12.7%] -top-[-2%] w-[24%] h-[28.5%] object-contain"
       />
       <img
         src="/purple_d.png"
         alt=""
-        className="absolute left-0 -top-[-200px] w-[175px] h-[180px] object-contain"
+        className="absolute left-[-5.4%] -top-[-25%] w-[21%] h-[21.5%] object-contain"
       />
       <img
         src="/green.png"
         alt=""
-        className="absolute right-[-5px] top-[418px] w-[150px] h-[160px] object-contain rotate-270"
+        className="absolute right-[-1.7%] top-[52.4%] w-[150px] h-[160px] object-contain rotate-270"
       />
       <img
         src="/purple_d.png"
         alt=""
-        className="absolute right-[-5px] top-[566px] w-[185px] h-[200px] object-contain rotate-270"
+        className="absolute right-[-0.5%] top-[70.3%] w-[190px] h-[200px] object-contain rotate-270"
       />
 
-      <h1 className="text-6xl font-bold text-center text-blue-700 m-8">
+      <h1
+        className="text-6xl font-bold text-center text-blue-700 m-8"
+        style={{ color: darkMode ? "#FFFFFF" : "#1D4ED8" }}
+      >
         Gallery
       </h1>
 
